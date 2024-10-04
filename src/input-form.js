e3f9f980367a9d8204b11cd0b8dd1a3ccc19360f
Daniel <dandcalvo@gmail.com>
2024-10-04 16:41:35 -0700

    Form validation start

--- src/input-form.js
+++ src/input-form.js
@@ -4,6 +4,7 @@ const button = document.getElementById('import');
 const api_key = document.getElementById('api_key');
 const board_id = document.getElementById('board_id');
 const data_file = document.getElementById('data_file');
+const form = document.querySelector('form');
 
 button.addEventListener('click', async () => {
     const API_KEY = api_key.value;
@@ -11,6 +12,10 @@ button.addEventListener('click', async () => {
     const dataFile = data_file.files[0];
 
     try {
+        if (!form.checkValidity()) {
+            throw new Error('Form is incomplete.');
+        }
+
         await startImport(API_KEY, BOARD_ID, dataFile);
     } catch (error) {
         throw error;
