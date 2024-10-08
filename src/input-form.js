6d134c0e0302212b64c347290b87b6a52f872d4a
Daniel <dandcalvo@gmail.com>
2024-10-08 11:23:00 -0700

    File type validation

--- src/input-form.js
+++ src/input-form.js
@@ -16,8 +16,27 @@ button.addEventListener('click', async (e) => {
             throw new Error('Form is incomplete.');
         }
 
+        if (!validateFileType(dataFile)) {
+            throw new Error('Invalid file type.');
+        }
+
         await startImport(API_KEY, BOARD_ID, dataFile);
     } catch (error) {
         console.error(error);
     }
 });
+
+function validateFileType(file) {
+    const validTypes = ['csv'];
+
+    const fileName = file.name;
+    const fileType = fileName.split('.')[1];
+
+    for (let type of validTypes) {
+        if (fileType === type) {
+            return true;
+        }
+    }
+
+    return false;
+}
