90a972074f21680fb7cdc672e27afe5a4e061a34
Daniel <dandcalvo@gmail.com>
2024-10-11 12:39:19 -0700

    form links

--- src/input-form.js
+++ src/input-form.js
@@ -6,6 +6,44 @@ const board_id = document.getElementById('board_id');
 const data_file = document.getElementById('data_file');
 const form = document.querySelector('form');
 
+const disclaimerLink = document.getElementById('disclaimer');
+const disclaimerPop = document.querySelector('.pop.disclaimer');
+const clickBackdrops = document.querySelectorAll('.click-backdrop');
+const instructionsLink = document.getElementById('instructions');
+const instructionsPop = document.querySelector('.pop.instructions');
+
+(() => {
+    disclaimerLink.addEventListener('click', () => {
+        disclaimerPop.classList.remove('hidden');
+        disableBodyScroll();
+    });
+
+    instructionsLink.addEventListener('click', () => {
+        instructionsPop.classList.remove('hidden');
+        disableBodyScroll();
+    });
+
+    clickBackdrops.forEach((backdrop) => {
+        backdrop.addEventListener('click', (e) => {
+            if (e.target.classList.contains('click-backdrop')) {
+                backdrop.classList.add('hidden');
+            }
+
+            disableBodyScroll(false);
+        });
+    });
+})();
+
+function disableBodyScroll(disable = true) {
+    const body = document.querySelector('body');
+
+    if (disable) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+    }
+}
+
 button.addEventListener('click', async (e) => {
     const API_KEY = api_key.value;
     const BOARD_ID = board_id.value;
